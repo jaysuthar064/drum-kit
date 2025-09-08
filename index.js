557e5ae271ced8489c10ad6de6829de57a582375
Jayanti Lal <jayantisuthar094@gmail.com>
2025-09-08 20:11:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var numberOfClicks=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfClicks;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+
+
+function handleClick(){
+    var buttonInnerHtml= this.innerHTML;
+    makesound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+  
+   
+}
+document.addEventListener("keypress",function(event) {
+        makesound(event.key);
+        buttonAnimation(event.key);
+});
+}
+
+
+function makesound(key){
+         switch (key) {
+    case "w":
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+   case "a":
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    case "s":
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+    case "d":
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+    case "j":
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+    case "k":
+             var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();  
+            break;
+    case "l":
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();    
+            break;                                  
+    default:
+        
+   }
+}
+
+
+function buttonAnimation(currentKey){
+       var activeBUtton= document.querySelector("."+currentKey);
+        activeBUtton.classList.add("pressed"); 
+        setInterval(function(){
+                activeBUtton.classList.remove("pressed");
+        },150);
+}
+
+
